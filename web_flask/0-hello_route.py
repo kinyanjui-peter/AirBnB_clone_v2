31ddad458931501e96dd879fcd25ef82dd9a25dd
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-12-30 01:57:06 +0300

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-#A script that starts Flask web application
+""" A script that starts Flask web application
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    You must use the option strict_slashes=False in your route definition
+    """
 from flask import Flask
 
 app = Flask("__main__")
+""" Flask class object"""
 
 @app.route('/display' , strict_slashes=False)
+
 def funct_to_display():
+    """function that displays Hello HBNB!"""
     return "Hello HBNB!"
+
+#prevent the script from running when called#
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
