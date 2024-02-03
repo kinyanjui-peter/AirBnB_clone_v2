524ee3a61050dbaf9542a4577f7188e9fa5185dc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-02-03 10:09:25 +0300

    file complession to tar/gtz file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,16 +5,18 @@ from datetime import datetime
 import os
 
 def do_pack():
-    """ generate a content folder"""
-    local("mkdir -p versions")
-    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    archiveName = f"web_static_{time}.tgz"
-    archivePath = os.path.join("versions", archiveName)
+  """ generate a content folder"""
+  local("mkdir -p versions")
+  time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+  archiveName = f"web_static_{time}.tgz"
+  archivePath = os.path.join("versions", archiveName)
   
-    final = local(f"tar czvf {archivePath} web_static")
+  final = local(f"tar czvf {archivePath} web_static")
   
-    if final.succeeded:print(f"{archivePath}")
-        return archivePath
-    else:
-        print("None")
-        return (final)
+  if final.succeeded:
+    print(f"{archivePath}")
+    return archivePath
+  else:
+    print("None")
+    return (final)
+ 
