03b4b9c694cc6b1e56feaac3ce10039b4d2b2b44
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-02-03 15:51:17 +0300

    Deploy Archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ from datetime import datetime
 import os
 """connection to server"""
 env.hosts = ["18.209.225.212", "34.207.58.78"]
-env.user = ubuntu
-env.key_filename = ssh -i .ssh/school
+env.user = "ubuntu"
+env.key_filename = "/home/peter/.ssh/school"
 
 
 def do_pack():
@@ -14,31 +14,31 @@ def do_pack():
     local("mkdir -p versions")
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     archiveName = f"web_static_{time}.tgz"
-    archivePath = os.path.join("versions", archiveName)
+    archive_path = os.path.join("versions", archiveName)
 
-    final = local(f"tar czvf {archivePath} web_static")
+    final = local(f"tar czvf {archive_path} web_static")
 
     if final.succeeded:
-        print(f"{archivePath}")
-        return archivePath
+        print(f"{archive_path}")
+        return archive_path
     else:
         print("None")
         return (final)
 
 
 
-def do_deploy(archivePath):
+def do_deploy(archive_path):
     """Distribute an archive to your web servers"""
     try:
-        if not os.path.exists(archivePath):
+        if not os.path.exists(archive_path):
             print("False: Archive file does not exist.")
             return False
 
         # Extract filename without extension from the archive path
-        extractedArchivePath = os.path.splitext(os.path.basename(archivePath))[0]
+        extractedArchivePath = os.path.splitext(os.path.basename(archive_path))[0]
 
         # Upload the archive to the tmp directory
-        put(archivePath, '/tmp/')
+        put(archive_path, '/tmp/')
         run(f"sudo mkdir -p /data/web_static/releases/{extractedArchivePath}")
         run(f"sudo tar -xzvf /tmp/{extractedArchivePath}.tgz -C /data/web_static/releases/")
 
