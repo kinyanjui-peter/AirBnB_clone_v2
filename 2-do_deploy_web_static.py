a3f6982b3e46d2b447a78b5d63e0a5a537de67bf
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-02-03 12:45:01 +0300

    Deploy Archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,31 +26,31 @@ def do_pack():
 
 def do_deploy(archivePath):
     """distributes an archive to your web servers"""
+    try:
+        if not os.path.exists(archivePath):
+            """check if the filepath exixt"""
+            print("False")
+            return False
+        # extract file from archivepath without filename
+        extractedArchivePath = os.path.splitext(os.path.basename(archivePath))[0]
     
-    if not os.path.exists(archivePath):
-        """check if the filepath exixt"""
+        # upload the acrchive to the tmp directory  
+        put(archivePath, '/tmp/')
+        run(f"sudo mkdir -p {extractedArchivePath}")
+        run(f"sudo tar -xzvf /tmp/{extractedArchivePath}.tgz -C /data/web_static/releases/extractedArchivePath")
+
+        # delete the archive from server 
+        run(f"sudo rm {extractedArchivePath}.tgz")
+    
+        # delete symbolic link 
+        run(f"sudo rm -f /data/web_static/current")
+    
+        # create new symbolic link
+        run(f"sudo ln -s /data/web_static/releases/{extractedArchivePath} /data/web_static/current")
+    
+        print("True")
+        return True
+
+    except Exception as e:
         print("False")
         return False
-    # extract file from archivepath without filename
-    extractedArchivePath = os.path.basename(archivePath).split('0')[0]
-    
-    # upload the acrchive to the tmp directory  
-    upload = put("archivePath" /tmp/)
-    run(f"sudo mkdir -p extractedArchivePath")
-    run(f"sudo tar -xzvf versions/web_static_20240203065022.tgz -C /data/web_static/releases/extractedArchivePath")
-
-    # delete the archive from server 
-    run(f"sudo rm {extractedArchivePath}.tgz")
-    
-    # delete symbolic link 
-    run(f"sudo rm -f /data/web_static/current")
-    
-    # create new symbolic link
-    run(f"sudo ln -s /data/web_static/releases/{extractedArchivePath} /data/web_static/current")
-    
-    print("true")
-    return true
-
-except Exception as e:
-    print("False")
-    return False
