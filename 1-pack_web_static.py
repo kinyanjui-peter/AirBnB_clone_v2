79e759a5124ec647a3b5eb7c637c49f1422ce3d8
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-02-03 10:01:17 +0300

    tcreate a tar/gtz file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+  """ generate a content folder"""
+  local("mkdir -p versions")
+  time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+  archiveName = f"web_static_{time}.tgz"
+  archivePath = os.path.join("versions", archiveName)
+  
+  final = local(f"tar czvf {archivePath} web_static")
+  
+  if final.succeeded:
+    print(f"{archivePath}")
+    return archivePath
+  else:
+    print("None")
+    return (final)
+ 
